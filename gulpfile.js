7c2c4e7240693c3abcd555455d65076d5c014502
Vicky Leong <vleong2332@gmail.com>
2016-06-10 16:54:45 -0400

    Create super basic layout and dependencies

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+	browserify = require('browserify'),
+	source = require('vinyl-source-stream');
+
+
+gulp.task('default', ['watch']);
+
+
+gulp.task('watch', function() {
+	gulp.watch(['scripts/index.js', 'scripts/modules/*.js'], ['browserify']);
+});
+
+
+gulp.task('browserify', function() {
+	return browserify('./scripts/index.js')
+		.bundle()
+		.pipe(source('bundled.js'))
+		.pipe(gulp.dest('scripts'));
+});
--- scripts/bundled.js
+++ scripts/bundled.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Test = require('./modules/test').Test;
 
 window.App = {
@@ -34,3 +35,21 @@ document.addEventListener('DOMContentLoaded', function() {
     App.init();
 
 });
+
+},{"./modules/test":2}],2:[function(require,module,exports){
+'use strict';
+
+function Test() {
+
+    return {
+
+        a: "hello",
+
+        b: "sup",
+    };
+
+}
+
+exports.Test = Test;
+
+},{}]},{},[1]);
